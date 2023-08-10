ef9421c47d68d09bd8a042bce6516ef979fa2f0a
Harsha <harshagowtham30@gmail.com>
2023-08-10 17:42:48 +0530

    new commit

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000; // Change this to your desired port
 
 app.get('/api/data', (req, res) => {
-  res.json({ message: 'Hello from the backend!' });
+  res.json({ message: 'Hello from the our backend!' });
 });
 
 app.listen(port, () => {
